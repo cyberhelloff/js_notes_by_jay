5e45e774cff725e2775c5fd922d96593f2800b9b
cyberhelloff <167119575+cyberhelloff@users.noreply.github.com>
2024-04-29 08:46:49 +0000

    conversionOperation

---
+++ js_course/03_conversionOperation.js
@@ -0,0 +1,36 @@
+// js conversion operation here
+
+let score = 5 //type is number 
+
+let num2str = String(score) //Number converts to string datatype 
+
+console.log(typeof num2str); 
+
+let student = "9"
+let str2num = Number(student) //string convert to number.....
+console.log(typeof str2num);
+
+let isloggedIn = true
+
+let bol2str = String(isloggedIn) // boolean to string 
+
+console.log(typeof bol2str);
+
+let num = 23
+let num2bol = Boolean(num)// number to boolean 
+console.log(typeof num2bol);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+
+
+let login = 1
+let booleanlogin = Boolean(login) //it shows true output 
+console.log(booleanlogin);
+
+//"" => false
+
+// "jay" => true 
+
+
