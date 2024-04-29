080bc20bc46a60da3f92176e1b5e1e18815dd795
cyberhelloff <167119575+cyberhelloff@users.noreply.github.com>
2024-04-29 07:13:52 +0000

    datatypes

---
+++ js_course/02_js_datatypes.js
@@ -0,0 +1,39 @@
+"use strict"; //treats all js code as newer version 
+
+//javascript Data types ....
+
+// alert(3 + 3) it wont work //we are using node js , not browser ...
+
+console.log(3
+            +3) //code readability should be high 
+
+console.log("jay");
+
+let name = "jay" //This "" is called as a string 
+
+let age = 17 //This is said to be number 
+
+let isLoggedIn = false //this is called boolean..
+
+let state;
+
+// primitive data types :-
+
+// number => 2 to power 53 
+// bigint 
+// string => ""
+// boolean => true/false 
+// null => standalone value 
+// undefine => 
+// symbol => unique 
+
+// -------------------------------------------------------------------------------------------------------------------------------------------
+
+//object 
+
+console.log(typeof null); // object 
+
+console.log(typeof undefined); //undefine
+
+
+
